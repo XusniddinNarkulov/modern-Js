7fad91437d94b8204e457c32e728bfd1aee800c7
XusniddinNarkulov <narkulof@gmail.com>
2022-04-08 19:56:11 +0500

    08 apr 2022

---
+++ login.js
@@ -0,0 +1,10 @@
+// let a = 5;
+// let t = 20;
+
+// export { a, t as ti };
+
+// //export default
+// export default 30; //default export
+
+let a = 101;
+module.exports = { a };
---
+++ script.js
@@ -0,0 +1,39 @@
+//CommonJs
+const login = require('./login.js');
+console.log(login);
+
+//
+// import * as hammasi from './login.js';
+// console.log(hammasi.a); //5
+// console.log(hammasi.ti); //20
+
+// //defaultni import qilish
+// import def from './login.js'; //default o'zgaruvchini chaqirish
+// console.log(def); //30
+
+// let a = 10;
+// console.log(a); //10
+
+// //top-level await in modules
+
+// const country = await fetch('https://restcountries.com/v2/name/uzbekistan');
+// const countryJson = await country.json();
+// console.log(countryJson);
+
+// console.log('salom');
+
+// function sot() {
+//   let a = 100;
+//   function sotibOl(nomi, soni) {
+//     let b = 200;
+//     console.log('hello world');
+//     return b;
+//   }
+//   return { a, sotibOl };
+// }
+// console.log(sot().sotibOl());
+// console.log(sot().a);
+
+// let asd = sot();
+// console.log(asd.a);
+// console.log(asd.sotibOl());
